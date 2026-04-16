41543f719435ba754594cdf70b3ef56392f0b44f
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-04-15 22:20:28 -0300

    Iniciando o repositorio

---
+++ src/main/java/myFinances/apiFinaces/ApiFinacesApplication.java
@@ -0,0 +1,13 @@
+package myFinances.apiFinaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myFinances/apiFinaces/ApiFinacesApplicationTests.java
@@ -0,0 +1,13 @@
+package myFinances.apiFinaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
